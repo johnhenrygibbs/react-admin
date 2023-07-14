bc03d7c56da9bb70e378c139ac91a09edaa5257c
John Gibbs <hello@johnhenrygibbs.com>
2023-07-13 18:06:39 -0700

    Work on theme.js

--- src/theme.js
+++ src/theme.js
@@ -122,3 +122,95 @@ export const tokens = (mode) => ({
 });
 
 // MUI Theme Settings
+
+export const themeSettings = (mode) => {
+    const colors = token(mode);
+
+    return {
+        palette: {
+            mode: mode,
+            ...(mode === 'dark'
+            ? {
+                primary: {
+                    main: colors.primary[500],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100],
+                },
+                background: {
+                    default: colors.primary[500],
+                }
+            } : {
+                primary: {
+                    main: colors.primary[100],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100],
+                },
+                background: {
+                    default: "#fcfcfc",
+                }
+            }),
+        },
+        typography: {
+            fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 14,
+            },
+        },
+    };
+};
+
+// Context for Color Mode
+
+export const ColorModeContext = createContext({
+    toggleColorMode: () => {}
+});
+
+export const useMode = () => {
+    const [mode, setMode] = useState("dark");
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () =>
+            setMode((prev) => (prev === "light" ? "dark" : "light")),
+        }),
+        []
+    );
+
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+    return [theme, colorMode];
+}
